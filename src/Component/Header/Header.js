1b74d50c70ac11364fc4d108851dc9e609beb038
Rasel Mia <ahmedrussell437@gmail.com>
2022-04-03 21:48:32 +0600

    set router

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Link to="/home">Home</Link>
+            <Link to="/reviews">Reviews</Link>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/blogs">Blogs</Link>
+            <Link to="/about">About</Link>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Component/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <h1>i am from not found</h1>
+        </div>
+    );
+};
+
+export default PageNotFound;

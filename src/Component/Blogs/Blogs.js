d7821df5d5835c253f3246135957b0c7ed069827
Rasel Mia <ahmedrussell437@gmail.com>
2022-04-04 20:15:53 +0600

    complete bannar

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import "./Blogs.css"
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>i am from blogs</h1>
+        <div className='blogs-container'>
+            <div className='blogs'>
+                <h1>What is contex api?</h1>
+                <p>The React Context API is a way for a React app to     effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.
+
+                Context API is a (kind of) new feature added in version 16.3 of React that allows one to share state across the entire app (or part of it) lightly and with ease</p>
+            </div>
+            <div className='blogs'>
+                <h1>what is symantic tag?</h1>
+                <p>Semantic HTML or semantic markup is HTML that introduces meaning to the web page rather than just presentation. For example, a p tag indicates that the enclosed text is a paragraph. This is both semantic and presentational because people know what paragraphs are, and browsers know how to display them.
+
+                On the flip side of this equation, tags such as b and i are not semantic. They define only how the text should look (bold or italic), and don't provide any additional meaning to the markup.</p>
+            </div>
         </div>
     );
 };
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -4,14 +4,23 @@ import "./Home.css"
 const Home = () => {
     return (
         <div>
-            <div className='banner-container'>
+             <section>
+             <div className='banner-container'>
                 <div className="home-conaiten-left">
-                    <h1>hello</h1>
+                    <h1>Welcome to T-shirt Zone</h1>
+                    <p>We are ready to give best product with best price.Scoldown and purchese your favourte product.Happy shopping!</p>
+                    <button>Buy Now</button>
                 </div>
                 <div className="home-container-right">
                     <img src={require("../img/t-shirt-vert-aventure-col-rond-pey407t1tfct0ne81ogobmigygi0tgib5h3zwzco7c.jpeg")} alt=""/>
                 </div>
             </div>
+             </section>
+            <section>
+                <div className="review-container">
+                    <h1>Custom Review(3)</h1>
+                </div>
+            </section>
         </div>
     );
 };
